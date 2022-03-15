96662ce510b0c59d49cc45d818cf7eeb39450d29
Sergey <anischenko99@gmail.com>
2022-03-15 20:30:27 +0300

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 app.use(express.static("./build"))
-app.listen(3000, () => {
+const PORT = process.env.PORT | 3000
+app.listen(PORT, () => {
     console.log(`3000 server started`);
   });
