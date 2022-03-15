0aa1fe540ead5636ab5eede475d7482be0904d82
Sergey <anischenko99@gmail.com>
2022-03-15 20:40:45 +0300

    fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const path = require("path")
 app.use(express.static(path.join(__dirname, './build')))
 const PORT = process.env.PORT | 3000
-app.get()
 app.listen(PORT, () => {
     console.log(`3000 server started`);
   });
